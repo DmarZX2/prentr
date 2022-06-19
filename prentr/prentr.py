8e371593f2374e923111cb94931c12ffa7bdf4a4
DmarZX2 <hamad.alaf123@gmail.com>
2022-06-19 10:16:52 +0400

    cleaning

--- prentr/prentr.py
+++ prentr/prentr.py
@@ -3,9 +3,3 @@ class prentr:
     def __setattr__(self,_, *value):
         print(', '.join(map(str, *value)) if isinstance(value[0], tuple) else value[0])
 
-
-#
-# prentr().prentr = 45,
-#
-# with open(r'C:\Users\Hamad\PycharmProjects\uaecinema-api\tests\she.html', 'r') as f:
-#     prentr().prentr = f.read()
